99935b8ca771ebaf60f51dd1ef45117dd26fa2d7
Andrew Font <andrew@mantiumai.com>
2022-06-01 17:40:18 -0400

    fixed linter error

--- apps/website/pages/gate/create.tsx
+++ apps/website/pages/gate/create.tsx
@@ -1,6 +1,7 @@
+import { TOKENS } from '@gateway/theme';
+
 import { Box, Stack, Typography } from '@mui/material';
 
-import { TOKENS } from '@gateway/theme';
 import { DashboardTemplate } from '../../components/templates/dashboard';
 
 export default function CreateGateway() {
@@ -22,30 +23,28 @@ export default function CreateGateway() {
           justifyContent="space-between"
           alignItems="flex-start"
           gap={6}
-        > 
+        >
           <Box sx={{ width: '25%' }}>
             <Typography variant="h5">Details</Typography>
             <Typography variant="caption">
               Lorem ipsum dolor sit amet
             </Typography>
           </Box>
-          <Box sx={{ width: '75%' }}>
-          </Box>
+          <Box sx={{ width: '75%' }}></Box>
         </Stack>
         <Stack
           direction="row"
           justifyContent="space-between"
           alignItems="flex-start"
           gap={6}
-        > 
+        >
           <Box sx={{ width: '25%' }}>
             <Typography variant="h5">Tasks</Typography>
             <Typography variant="caption">
               Lorem ipsum dolor sit amet
             </Typography>
           </Box>
-          <Box sx={{ width: '75%' }}>
-          </Box>
+          <Box sx={{ width: '75%' }}></Box>
         </Stack>
       </Stack>
     </DashboardTemplate>
