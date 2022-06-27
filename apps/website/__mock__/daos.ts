dd4de82c33814f10bc45c9f2cb5d87e5cea486aa
Ashish Goyal <>
2022-06-27 18:23:36 +0530

    Change some skill set

--- apps/website/__mock__/daos.ts
+++ apps/website/__mock__/daos.ts
@@ -22,14 +22,14 @@ export const temporaryKnowledge = [
   { key: 8, label: 'Don' }
 ]
 export const temporarySkills = [
-  { key: 1, label: 'The Shawshank Redemption' },
-  { key: 2, label: 'The Godfather' },
-  { key: 3, label: 'The Godfather: Part II' },
-  { key: 4, label: 'The Dark Knight' },
-  { key: 5, label: '12 Angry Men' },
-  { key: 6, label: "Schindler's List" },
-  { key: 7, label: 'Pulp Fiction' },
-  { key: 8, label: 'Don' }
+  { key: 1, label: 'Acquisition skills' },
+  { key: 2, label: 'Analytical ability' },
+  { key: 3, label: 'Adaptability' },
+  { key: 4, label: 'Execution willingness' },
+  { key: 5, label: 'Resilience' },
+  { key: 6, label: 'Advice ability' },
+  { key: 7, label: 'Relationship Management' },
+  { key: 8, label: 'Charisma' }
 ]
 export const temporaryAttitudes = [
   { key: 1, label: 'The Shawshank Redemption' },
